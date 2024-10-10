9b69672d16379fb848396ada09bcda6ed18f4907
Philip Giuliani <philip.giuliani@keepinmind.info>
2024-10-10 14:30:25 +0200

    Add typespec

--- lib/ex_lang/locale.ex
+++ lib/ex_lang/locale.ex
@@ -1,4 +1,10 @@
 defmodule ExLang.Locale do
+  @type t :: %ExLang.Locale{
+    code: String.t(),
+    script: String.t() | nil,
+    territory: String.t() | nil,
+  }
+  
   @enforce_keys [:code]
   defstruct [:code, :script, :territory]
 end
